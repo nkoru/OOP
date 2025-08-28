f829334369a0849191388a01c9ed4085d0256125
nkoru <niccucarvellida@gmail.com>
2025-08-28 23:26:00 +0800

    Program

---
+++ Ubod_OOPConcepts/Ubod_OOPConcepts/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_OOPConcepts
+{
+    internal class Program
+    {
+        // Name: Beatrice Nicole Ubod
+        // Section: IT306
+        // Exercise: OOP Concepts
+
+        static void Main(string[] args)
+        {
+            // Create PoloShirt object
+            Shirt polo = new PoloShirt()
+            {
+                Brand = "Nike",
+                Size = "M",
+                HasCollar = true
+            };
+
+            // Create TShirt object
+            Shirt tshirt = new TShirt()
+            {
+                Brand = "Uniqlo",
+                Size = "L",
+                SleeveLength = "Short"
+            };
+
+            // Polymorphic calls to overridden method
+            polo.DisplayInformation();
+            tshirt.DisplayInformation();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Ubod_OOPConcepts/Ubod_OOPConcepts/Shirt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_OOPConcepts
+{
+    // Abstract base class - abstraction
+    internal abstract class Shirt
+    {
+        public string Brand { get; set; }
+        public string Size { get; set; }
+
+        // Abstract method to be implemented by derived classes
+        public abstract void DisplayInformation();
+    }
+
+    // Derived class PoloShirt - inheritance
+    internal class PoloShirt : Shirt
+    {
+        public bool HasCollar { get; set; }
+
+        public override void DisplayInformation()
+        {
+            Console.WriteLine("\nPolo Shirt Details:");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Has Collar: {(HasCollar ? "Yes" : "No")}");
+        }
+    }
+
+    // Derived class TShirt - inheritance
+    internal class TShirt : Shirt
+    {
+        public string SleeveLength { get; set; }
+
+        public override void DisplayInformation()
+        {
+            Console.WriteLine("\nT-Shirt Details:");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Sleeve Length: {SleeveLength}");
+        }
+    }
+}
