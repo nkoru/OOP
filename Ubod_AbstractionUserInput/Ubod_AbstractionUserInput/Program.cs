459d9d14e9f6e6b86f3d7cf2cd6584dbd03faf1a
nkoru <niccucarvellida@gmail.com>
2025-08-28 22:49:20 +0800

    Program

---
+++ Ubod_AbstractionUserInput/Ubod_AbstractionUserInput/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_AbstractionUserInput
+{
+    internal class Program
+    {
+        // Name: Beatrice Nicole Ubod
+        // Section: IT306
+        // Exercise: Abstraction with Shirt Class (User Input)
+
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+
+            Console.WriteLine("\tShirt Information Input");
+
+            Console.Write("Enter the brand of the shirt: ");
+            string brandInput = Console.ReadLine();
+
+            Console.Write("Enter the size of the shirt: ");
+            string sizeInput = Console.ReadLine();
+
+            CustomShirt myShirt = new CustomShirt
+            {
+                Brand = brandInput,
+                Size = sizeInput
+            };
+
+            myShirt.DisplayInformation();
+
+            Console.Write("\nDo you want to enter another shirt? (y/n): ");
+            char answer = Console.ReadKey().KeyChar;
+
+            if (answer == 'y' || answer == 'Y')
+                goto Main;
+            else if (answer == 'n' || answer == 'N')
+                return;
+            else
+            {
+                Console.WriteLine("\nInvalid input. Exiting...");
+                return;
+            }
+        }
+    }
+}
---
+++ Ubod_AbstractionUserInput/Ubod_AbstractionUserInput/Shirt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_AbstractionUserInput
+{
+    // Abstract base class
+    internal abstract class Shirt
+    {
+        public string Brand { get; set; }
+        public string Size { get; set; }
+
+        // Abstract method to display info
+        public abstract void DisplayInformation();
+    }
+
+    // Concrete class implementing the abstract Shirt
+    internal class CustomShirt : Shirt
+    {
+        public override void DisplayInformation()
+        {
+            Console.WriteLine("\nShirt Details:");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+        }
+    }
+}
