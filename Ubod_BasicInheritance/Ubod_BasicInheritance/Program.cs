e579a059a8f4c2aeb3ddde844e51b349f34c27b7
nkoru <niccucarvellida@gmail.com>
2025-08-28 22:19:20 +0800

    Program

---
+++ Ubod_BasicInheritance/Ubod_BasicInheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_BasicInheritance
+{
+    internal class Program
+    {
+        // Name: Beatrice Nicole Ubod
+        // Section: IT306
+        // Exercise: Inheritance with Shirt and PoloShirt Classes
+
+        static void Main(string[] args)
+        {
+            // Instantiate and set properties directly
+            PoloShirt myPolo = new PoloShirt()
+            {
+                Brand = "Adidas",
+                Size = "L",
+                HasCollar = true
+            };
+
+            // Display the details
+            myPolo.DisplayInformation();
+
+            // Wait for user to press a key before closing
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
