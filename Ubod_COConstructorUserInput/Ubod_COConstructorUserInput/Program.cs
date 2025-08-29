7ecd4f0afc94644090472cd4b89bde2e9763a8e4
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2025-08-29 14:33:27 +0800

    Program

---
+++ Ubod_COConstructorUserInput/Ubod_COConstructorUserInput/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_COConstructorUserInput
+{
+    internal class Program
+    {
+        // Name: Beatrice Nicole Ubod
+        // Section: IT306
+        // Exercise Title: User Input with Constructor
+
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+            // prompt to let the users know what is the program all about
+
+            Console.WriteLine("\tshis program will ask you about your shoe collection/s");
+            Console.Write("\nEnter the brand of your shoes: "); 
+            string aBrand = Console.ReadLine();
+
+            Console.Write("Enter the color of your shoes: "); 
+            string aColor = Console.ReadLine();
+
+            Console.Write("Enter the size of your shoes: "); 
+            int aSize = int.Parse(Console.ReadLine());
+
+            // instantiation of the class object
+            Shoes shoes1 = new Shoes(aBrand, aColor, aSize);
+
+            // calling of the method to display
+            shoes1. DisplayShoeDetails();
+
+            // input another shoe details
+            Console.Write("\nDo you want to add more (y/n)? "); 
+            char ans = Convert.ToChar(Console.ReadLine());
+            if (ans == 'y')
+            {
+                goto Main;
+            }
+            else if (ans == 'n')
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Ubod_COConstructorUserInput/Ubod_COConstructorUserInput/Shoes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ubod_COConstructorUserInput
+{
+    internal class Shoes
+    {
+        // declaration of the fields
+        public string brand, color; public int size;
+
+        public Shoes(string aBrand, string aColor, int aSize) // parameterized constructor
+        {
+            brand = aBrand;
+            color = aColor;
+            size = aSize;
+        }
+
+        // method used to display its details
+        public void DisplayShoeDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("\t Shoe Details ----"); Console.WriteLine("\n\t/Shoe Brand: " + brand); Console.WriteLine("\tShoe Color: " + color); Console.WriteLine("\tShoe Size: * + size");
+
+        }
+    }
+}
