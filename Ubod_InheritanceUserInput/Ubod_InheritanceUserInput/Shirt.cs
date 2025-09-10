0b4d4218164855be2e6c4a255f9fede950e74d69
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2025-09-10 14:44:21 +0800

    Program

--- Ubod_InheritanceUserInput/Ubod_InheritanceUserInput/Shirt.cs
+++ Ubod_InheritanceUserInput/Ubod_InheritanceUserInput/Shirt.cs
@@ -8,28 +8,65 @@ namespace Ubod_InheritanceUserInput
 {
     internal class Shirt
     {
-        public string Brand { get; set; }
-        public string Size { get; set; }
+        public string brand;
+        public string size;
 
-        public virtual void DisplayInformation()
+        public Shirt (string shirtBrand, string shirtSize)
+        {
+            brand = shirtBrand;
+            size = shirtSize;
+        }
+
+        public void DisplayInformation()
         {
             Console.Clear();
             Console.WriteLine(Environment.NewLine);
+
             Console.WriteLine("Shirt Details:");
-            Console.WriteLine($"Brand: {Brand}");
-            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Brand: {brand}");
+            Console.WriteLine($"Size: {size}");
         }
     }
 
     // Derived class PoloShirt inherits Shirt
-    internal class PoloShirt : Shirt
+    class PoloShirt : Shirt
     {
         public bool HasCollar { get; set; }
+        public bool SleeveLength { get; set; }
 
-        public override void DisplayInformation()
+        public PoloShirt (string shirtBrand, string shirtSize, bool hasCollar, bool sleeveLength) : base (shirtBrand, shirtSize) 
+        {
+            HasCollar = hasCollar;
+            SleeveLength = sleeveLength;
+        }
+
+        public void PoloShirtDetails()
         {
             base.DisplayInformation();
             Console.WriteLine($"Has Collar: {(HasCollar ? "Yes" : "No")}");
+
+            Console.WriteLine($"Sleeve Length: {(SleeveLength ? "Short" : "Long")}");
+        }
+    }
+
+    //Another derived class Tshirt inherits Shirt
+    class Tshirt : Shirt
+    {
+        public bool CloseNeck { get; set; }
+        public bool VNeck { get; set; }
+
+        public Tshirt (string shirtBrand, string shirtSize, bool closeNeck, bool vNeck) : base (shirtBrand, shirtSize)
+        {
+            CloseNeck = closeNeck;
+            VNeck = vNeck;
+        }
+
+        public void TShirtDetails()
+        {
+            base.DisplayInformation();
+            Console.WriteLine($"Close Neck: {(CloseNeck ? "Yes" : "No")}");
+
+            Console.WriteLine($"Vneck: {(VNeck ? "Short" : "Long")}");
         }
     }
 }
