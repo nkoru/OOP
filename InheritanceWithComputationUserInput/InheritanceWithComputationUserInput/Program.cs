8c41597101311390137f52a4c45e1534ee4663a4
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2025-09-15 14:51:04 +0800

    Program

---
+++ InheritanceWithComputationUserInput/InheritanceWithComputationUserInput/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceWithComputationUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+
+            Console.Write("Chooose the type of shirt (1 for Polo Shirt, 2 for Tshirt): ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.Clear();
+                // Input details for PoloShirt
+                PoloShirt polo = new PoloShirt();
+
+                Console.Write("Enter Polo Shirt Brand: ");
+                polo.ShirtType = Console.ReadLine();
+
+                Console.Write("Enter Polo Shirt Price: ");
+                polo.Price = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Is the buyer a senior citizen? (yes/no): ");
+                string seniorInput = Console.ReadLine().ToLower();
+                polo.IsSeniorCitizen = (seniorInput == "yes");
+
+                Console.WriteLine("\n---- Polo Shirt ----\n");
+                polo.DisplayDetails();
+
+                Console.Write($"Final Price after Discount: {polo.ComputeDiscountedPrice()}\n");
+            }
+
+            else if (choice == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("\tTShirt Input");
+                // Input details for TShirt
+                TShirt tshirt = new TShirt();
+
+                Console.Write("\nEnter T-Shirt Brand: ");
+                tshirt.ShirtType = Console.ReadLine();
+
+                Console.Write("Enter T-Shirt Price: ");
+                tshirt.Price = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter quantity of T-Shirts to buy: ");
+                tshirt.Quantity = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\n---- T-Shirt ----\n");
+                tshirt.DisplayDetails();
+
+                Console.Write($"Final Price after Discount: {tshirt.ComputeDiscountedPrice()}\n");
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
+
+            //Prompt for user 
+            Console.Write("\nDo you want to enter another Shirt? (y/n): ");
+            char answer = Console.ReadKey().KeyChar;
+
+            if (answer == 'y' || answer == 'Y')
+                goto Main;
+            else if (answer == 'n' || answer == 'N')
+                return;
+            else
+            {
+                Console.WriteLine("\nInvalid input. Exiting...");
+                return;
+            }
+        }
+    }
+}
---
+++ InheritanceWithComputationUserInput/InheritanceWithComputationUserInput/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InheritanceWithComputationUserInput")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InheritanceWithComputationUserInput")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e54007f2-439a-4663-bfeb-8fed146d37af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
