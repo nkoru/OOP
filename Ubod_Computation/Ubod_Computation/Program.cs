23335b2d27345bc914bdc3ce835bfb5d86c92e58
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2025-09-24 14:35:59 +0800

    Program

---
+++ Ubod_Computation/Ubod_Computation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IrregularStudent irregularStudent = new IrregularStudent(6000, 12, 1500);
+            WorkingStudent workingStudent = new WorkingStudent(6000, 12500, 42000);
+
+            double irregularStudentTF = irregularStudent.ComputeTuitionFee();
+            double workingStudentTF = workingStudent.ComputeTuitionFee();
+
+            Console.WriteLine("Irregular Student Tuition Fee: " + irregularStudentTF);
+            Console.WriteLine("Working Student Tuition Fee: " + workingStudentTF);   
+        }
+    }
+}
---
+++ Ubod_Computation/Ubod_Computation/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_Computation
+{
+    class Student
+    {
+        public double MiscellaneousFee { get; set; }
+
+        public Student(double miscellaneousFee)
+        {
+            MiscellaneousFee = miscellaneousFee;
+        }
+
+    public virtual double ComputeTuitionFee()
+        {
+            return MiscellaneousFee;
+        }
+    }
+
+    class IrregularStudent : Student
+    {
+        public int NumberOfUnits {  get; set; }
+        public double RatePerUnit { get; set; }
+
+        public IrregularStudent(double miscellaneousFee, int numberOfUnit, double ratePerUnit) : base(miscellaneousFee)
+        {
+            NumberOfUnits = numberOfUnit;
+            RatePerUnit = ratePerUnit;
+        }
+
+    public override double ComputeTuitionFee()
+        {
+            double TuitionFee = NumberOfUnits * RatePerUnit;
+            return TuitionFee + MiscellaneousFee;
+        }
+    }
+
+    class WorkingStudent : Student
+    {
+        public double TuitionFeeDiscount { get; set; }
+        public double BaseTuitionFee {  get; set; }
+
+        public WorkingStudent(double miscellaneousFee, double tuitionFeeDiscount, double baseTuitionFee) : base(miscellaneousFee)
+        {
+            TuitionFeeDiscount = tuitionFeeDiscount;
+            BaseTuitionFee = baseTuitionFee;    
+        }
+
+        public override double ComputeTuitionFee()
+        {
+            double tuitionFee = BaseTuitionFee - TuitionFeeDiscount;
+            return tuitionFee + MiscellaneousFee;
+        }
+    }
+}
+
