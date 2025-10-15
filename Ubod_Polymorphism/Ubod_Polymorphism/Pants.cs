7ffd8d208559b6d1bd15d738a24bfef10b481561
nkoru <niccucarvellida@gmail.com>
2025-10-15 19:36:46 +0800

    Program

---
+++ Ubod_Polymorphism/Ubod_Polymorphism/Pants.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_Polymorphism
+{
+    internal class Pants
+    {
+        public string Brand { get; set; }
+        public string Size { get; set; }
+
+        // Virtual method to display info (can be overridden by derived classes)
+        public virtual void DisplayInformation()
+        {
+            Console.WriteLine("\n---- Pants Details ----");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+        }
+    }
+
+    // Derived class: Jeans
+    internal class Jeans : Pants
+    {
+        public string FitType { get; set; } //Skinny, Slim, Regular
+
+        // Override base class method
+        public override void DisplayInformation()
+        {
+            Console.WriteLine("\n--- Jeans Details ---");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Fit Type: {FitType}");
+        }
+    }
+
+    // Derived class: Sweatpants
+    internal class Sweatpants : Pants
+    {
+        public string Material { get; set; } //Cotton, Fleece, Polyester
+
+        // Override base class method
+        public override void DisplayInformation()
+        {
+            Console.WriteLine("\n--- Sweatpants Details ---");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Material: {Material}");
+        }
+    }
+}
+
---
+++ Ubod_Polymorphism/Ubod_Polymorphism/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubod_Polymorphism
+{
+    internal class Program
+    {
+        // Name: Beatrice Nicole Ubod
+        // Section: IT306
+        // Exercise: Polymorphism
+
+        static void Main(string[] args)
+        {
+            // Using polymorphism with base class reference
+            Pants myPants;
+
+            // Jeans instance assigned to Pants reference
+            myPants = new Jeans()
+            {
+                Brand = "Beepop",
+                Size = "26",
+                FitType = "Baggy Fit"
+            };
+
+            myPants.DisplayInformation(); // Calls Jeans' overridden method
+
+            // Sweatpants instance assigned to Pants reference
+            myPants = new Sweatpants()
+            {
+                Brand = "Adidas",
+                Size = "Medium",
+                Material = "Cotton"
+            };
+
+            myPants.DisplayInformation(); // Calls Sweatpants' overridden method
+
+            Console.ReadKey();
+        }
+    }
+}
+
